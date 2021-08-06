b4a4794f818c3cf4784bf52dcff20043ac938043
Chris Oliver <excid3@gmail.com>
2021-08-06 10:48:27 -0500

    Set root route

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,6 @@ end
   resources :notifications, only: [:index]
   resources :announcements, only: [:index]
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-  root to: 'home#index'
+  root to: 'addresses#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
